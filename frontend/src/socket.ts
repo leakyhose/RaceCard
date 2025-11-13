ab761f9b96d9333c9fc459e375ca60dbe1b4c7bc
leakyhose <yimingsu2007@gmail.com>
2025-11-12 22:37:59 -0800

    Add socket.ts to have a singular socket connection

---
+++ frontend/src/socket.ts
@@ -0,0 +1,17 @@
+import { io, Socket } from "socket.io-client"
+import type {
+  ServerToClientEvents,
+  ClientToServerEvents,
+} from "../../shared/types"
+
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  "http://localhost:3000"
+)
+
+socket.on("connect", () => {
+  console.log("Connected to backend:", socket.id)
+})
+
+socket.on("disconnect", () => {
+  console.log("Disconnected from backend")
+})
